58d35e4e24a5549cc698fe44e9b887ffff6444e8
Lucas Yang <lyang420@umd.edu>
2024-11-10 17:54:35 -0500

    added preliminary tests for date filtering by UI and URL

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,38 @@
+
+/* `example.spec.ts` is the default test file generated after installing
+Playwright. For our purposes, we'll write our functionality tests in this file.
+
+Important: Make sure to run `npm run dev` in the command line and wait for the
+page to launch before running tests, otherwise tests will access a blank
+localhost port and fail. */
+
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('verify date filtering for UI', async ({ page }) => {
+	await page.goto('http://localhost:3000/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+	const startDate = page.locator('div').filter({ hasText: /^Start Date$/ }).getByRole('textbox');
+	const endDate = page.locator('div').filter({ hasText: /^End Date$/ }).getByRole('textbox');
+
+	await expect(startDate).toHaveValue('2024-10-27');
+	await expect(endDate).toHaveValue('2024-10-29');
+	await expect(page.getByRole('main')).toContainText('Oct 27, 2024 - Oct 29, 2024');
+
+	await startDate.fill('2024-11-01');
+	await endDate.fill('2024-11-05');
+
+	await expect(startDate).toHaveValue('2024-11-01');
+	await expect(endDate).toHaveValue('2024-11-05');
+	await expect(page.getByRole('main')).toContainText('Nov 1, 2024 - Nov 5, 2024');
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('verify date filtering for URL', async ({ page }) => {
+	await page.goto('http://localhost:3000/?startDate=2024-11-01&endDate=2024-11-05');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+	const startDate = page.locator('div').filter({ hasText: /^Start Date$/ }).getByRole('textbox');
+	const endDate = page.locator('div').filter({ hasText: /^End Date$/ }).getByRole('textbox');
+	
+	await expect(startDate).toHaveValue('2024-11-01');
+	await expect(endDate).toHaveValue('2024-11-05');
+	await expect(page.getByRole('main')).toContainText('Nov 1, 2024 - Nov 5, 2024');
 });
