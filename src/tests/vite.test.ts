09f2a4777f89e4bd803b729e811f69c6e0ba3ebb
Lucas Yang <lyang420@umd.edu>
2024-11-12 21:30:34 -0500

    added initial test for data transformation

--- src/tests/vite.test.ts
+++ src/tests/vite.test.ts
@@ -1,5 +1,9 @@
-import { describe, it, expect } from "vitest";
-import { tryParseDateString, parseDateString } from "@/lib/utils";
+import { describe, it, expect } from 'vitest'
+import { tryParseDateString, parseDateString } from '@/lib/utils'
+import { getDeviceChartData } from '@/components/ProductionReport'
+
+/* Note for date parsing: consider also testing months that are single digit, or
+ambiguously written dates. */
 
 describe("tryParseDateString", () => {
    it("should correctly parse dates in the five listed formats", () => {
@@ -23,4 +27,21 @@ describe("parseDateString", () => {
    it("should throw an error for invalid strings", () => {
       expect(() => parseDateString("invalid")).toThrowError("Invalid date string: invalid");
    });
+});
+
+/* Note for data transformation: coonsider also testing number rounding and
+converting to different units (hours), empty input, etc. */
+
+describe("getDeviceChartData", () => {
+	it("should transform a given data record into chart format for the page", () => {
+		const summary = {
+			"active": {good: 100, reject: 200, duration: 10800 },
+			"inactive": { good: 50, reject: 150, duration: 7200 }
+		};
+		const ans = getDeviceChartData(summary);
+		expect(ans).toEqual([
+			{ state: 'active', good: 100, reject: 200, duration: 3.0 },
+			{ state: 'inactive', good: 50, reject: 150, duration: 2.0 }
+		]);
+	});
 });
