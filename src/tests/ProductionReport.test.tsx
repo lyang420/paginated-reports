aeccbe8ccf7aca74eb92570b423f1855b51ab7c3
Lucas Yang <lyang420@umd.edu>
2024-11-14 16:07:39 -0500

    added comments explaining defunct unit test

--- src/tests/ProductionReport.test.tsx
+++ src/tests/ProductionReport.test.tsx
@@ -1,11 +1,16 @@
 /* This unit test was placed here after much difficulty with import statements.
 This was by far the most cumbersome of the Vitest unit tests. */
 
-import React 												from "react";
-import { ProductionReport }                  	from "@/components/ProductionReport"
-import { render, screen }              from "@testing-library/react";
+import React 								from "react";
+import { ProductionReport         } from "@/components/ProductionReport"
+import { render, screen           } from "@testing-library/react";
 import { describe, expect, it, vi } from "vitest";
 
+/* This test was originally meant to test the way the application handles
+parameters present in the URL (similar to another test already in the
+Playwright suite), but it hilariously does not work (hangs forever in the
+terminal). It currently is not included in the workflow for that reason.
+Suspected cause: faulty mocking and rendering. */
 describe("Handle URL Parameters", () => {
 	vi.clearAllMocks();
 	vi.mock("next/navigation", () => ({
