f6a28cb380e81aa6c380ac73b3e342068276606a
Lucas Yang <lyang420@umd.edu>
2024-11-14 16:13:44 -0500

    added descriptive comments for vite unit tests

--- src/tests/vite.test.ts
+++ src/tests/vite.test.ts
@@ -1,10 +1,14 @@
-import { describe, it, expect } from 'vitest'
-import { tryParseDateString, parseDateString } from '@/lib/utils'
-import { getDeviceChartData } from '@/components/ProductionReport'
+/* `vite.test.ts` contains unit tests that make sure the utility functions used
+by the application work as they should. */
 
-/* Note for date parsing: consider also testing months that are single digit, or
-ambiguously written dates. */
+import { getDeviceChartData                  } from '@/components/ProductionReport'
+import { parseDateString, tryParseDateString } from '@/lib/utils'
+import { describe, expect, it                } from 'vitest'
 
+/* This test feeds the same date (October 27, 2024) in a number of commonly
+used formats into the `tryParseDateString()` function, as well as an invalid
+string, to make sure it can correctly differentiate between valid and invalid
+dates. */
 describe("tryParseDateString", () => {
    it("should correctly parse dates in the five listed formats", () => {
 		const ans = tryParseDateString("2024-10-27")?.toISOString();
@@ -12,11 +16,19 @@ describe("tryParseDateString", () => {
       expect(tryParseDateString("10/27/2024")?.toISOString()).toBe(ans);
       expect(tryParseDateString("10-27-2024")?.toISOString()).toBe(ans);
    });
+	/* Additional options include ambiguously worded dates (e.g., "5-1-2024",
+	May 1st, or January 5th?). */
    it("should return null for invalid strings", () => {
       expect(tryParseDateString("invalid")).toBeNull();
    });
 });
 
+/* This test feeds the same date (October 27, 2024) in a number of commonly
+used formats into the `parseDateString()` function, as well as an invalid
+string, to make sure it can correctly differentiate between valid and invalid
+dates. In fact, this utility function only makes a call to
+`tryParseDateString()` above, and its main use is to handle errors resulting
+from invalid date strings. */
 describe("parseDateString", () => {
 	it("should correctly parse date strings by calling tryParseDateString()", () => {
 		const ans = parseDateString("2024-10-27")?.toISOString();
@@ -29,9 +41,9 @@ describe("parseDateString", () => {
    });
 });
 
-/* Note for data transformation: coonsider also testing number rounding and
-converting to different units (hours), empty input, etc. */
-
+/* This test creates an example data record and feeds it to
+`getDeviceChartData()`, which is responsible for converting records into a
+format acceptable for table and chart generation. */
 describe("getDeviceChartData", () => {
 	it("should transform a given data record into chart format for the page", () => {
 		const summary = {
